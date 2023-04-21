34aa0b30633db17edb1a3ef73dfa3a8745cb7448
Elodie mongellaz <mongellaz.e@gmail.com>
2023-04-21 09:00:07 +0200

    feat: add method createUser in api.service

--- src/app/_service/api.service.ts
+++ src/app/_service/api.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+apiUrl: string;
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {
+    this.apiUrl = environment.apiBaseUrl;
+   }
+   
+   httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    })
+  };
+
+   createUser(user: any){
+    return this.httpClient.post(this.apiUrl + "/auth/signup/jeune",user, this.httpOptions)
+   }
 }
