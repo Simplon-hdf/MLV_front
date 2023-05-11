a258c6044a6d995f6db2a0bc6c1301c7bd429f5b
Elodie mongellaz <mongellaz.e@gmail.com>
2023-05-11 09:14:10 +0200

    refactor(router): Modify paths in appRouting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { RegisterComponent } from './register/register.component';
-import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './modules/home/home.component';
+import { RegisterComponent } from './modules/register/register.component';
+import { LoginComponent } from './modules/login/login.component';
+
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'login', component: LoginComponent },
+  { path: 'auth/signup/jeune', component: RegisterComponent },
+  { path: 'auth/login', component: LoginComponent },
 ];
 
 @NgModule({
