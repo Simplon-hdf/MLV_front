411e1d171e77b5f50eaab69e9c266599888c1411
Elodie mongellaz <mongellaz.e@gmail.com>
2023-05-12 08:23:20 +0200

    feat: add interceptor.module and remove http.interceptor

--- src/app/core/interceptor/http.interceptor.ts
+++
@@ -1,8 +0,0 @@
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { AuthInterceptor } from './auth.interceptor';
-import { ErrorInterceptor } from './error.interceptor';
-
-export const httpInterceptorProvider = [
-  { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
-  { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
-];
